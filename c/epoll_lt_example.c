b20412ba52b1315f341c9af6a6f3b598048ca85b
exfly <exflyg@gmail.com>
2020-03-25 09:30:46 +0800

    feat: 2st step

--- c/epoll_lt_example.c
+++ c/epoll_lt_example.c
@@ -1,19 +1,25 @@
 #include <stdio.h>     // for fprintf()
-#include <unistd.h>    // for close(), read()
-#include <sys/epoll.h> // for epoll_create1(), epoll_ctl(), struct epoll_event
-#include <string.h>    // for strncmp
-
-#include <stdio.h>     // for fprintf()
 #include <unistd.h>    // for close()
-#include <sys/epoll.h> // for epoll_create1()
+#include <sys/epoll.h> // for epoll_create1(), epoll_ctl(), struct epoll_event
 
 int main()
 {
+    struct epoll_event event;
     int epoll_fd = epoll_create1(0);
 
     if (epoll_fd == -1)
     {
         fprintf(stderr, "Failed to create epoll file descriptor\n");
+        return 1;
+    }
+
+    event.events = EPOLLIN;
+    event.data.fd = 0;
+
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, 0, &event))
+    {
+        fprintf(stderr, "Failed to add file descriptor to epoll\n");
+        close(epoll_fd);
         return 1;
     }
 
